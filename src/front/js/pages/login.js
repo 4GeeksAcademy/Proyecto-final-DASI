fcdc7edbe4a7b1151591b21682aa1d7444c9be69
DCardigo <123099651+DCardigo@users.noreply.github.com>
2023-08-16 11:18:33 +0000

    prueba

---
+++ src/front/js/pages/login.js
@@ -0,0 +1,35 @@
+import React, { useContext } from "react";
+import { Context } from "../store/appContext";
+import "../../styles/home.css";
+
+export const Login = () => {
+	const { store, actions } = useContext(Context);
+
+	return (
+		<div className=" text-center bg-success bg-opacity-25">
+
+            <h1 id="log" >Login</h1>
+
+            <div className="col-6 m-auto" >
+
+                <div className="input-group my-4 ">
+                <input type="email" className="form-control" placeholder="Email" aria-label="Recipient's username" aria-describedby="button-addon2"/>
+                </div>
+
+                <div className="input-group">
+                <input type="password" className="form-control" placeholder="Contraseña" aria-label="Recipient's username" aria-describedby="button-addon2"/>
+                </div>
+
+                <input class="btn btn-submit mt-5" type="submit" value="Submit"></input>
+
+                <br/>
+                <br/>
+
+                <a href="#" class="link-primary">Cre</a>
+
+            </div>
+
+
+		</div>
+	);
+};
