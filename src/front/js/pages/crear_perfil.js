0941669d5d5ec864a895ef84221e0a6e72cafd1c
Sergio Reverte <129745342+Reverte10@users.noreply.github.com>
2023-08-16 10:57:29 +0000

    crear vista crear perfil

--- src/front/js/pages/crear_perfil.js
+++ src/front/js/pages/crear_perfil.js
@@ -1,5 +1,4 @@
 import React, { useState } from "react";
-import { Link } from "react-router-dom";
 
 export const CrearPerfil = () => {
     const [contact, setContact] = useState({
@@ -36,110 +35,112 @@ export const CrearPerfil = () => {
 
     return (
         <div className="container">
-            <div>
-                <h1 className="text-center mt-5">Crear perfil</h1>
-                <form onSubmit={createContact}>
-                    <div className="form-group">
-                        <label>Nombre</label>
-                        <input
-                            type="text"
-                            onChange={handleChange}
-                            className="form-control"
-                            placeholder="Añada aquí su nombre"
-                            name="nombre"
-                        />
-                    </div>
-                    <div className="form-group">
-                        <label>Apellidos</label>
-                        <input
-                            type="text"
-                            onChange={handleChange}
-                            className="form-control"
-                            placeholder="Añada aquí sus apellidos"
-                            name="apellidos"
-                        />
-                    </div>
-                    <div className="form-group">
-                        <label>Telefono</label>
-                        <input
-                            type="phone"
-                            onChange={handleChange}
-                            className="form-control"
-                            placeholder="Añada aquí el telefono"
-                            name="telefono"
-                        />
-                    </div>
+            <div className="row justify-content-center">
+                <div className="col-md-8">
+                    <h1 className="text-center mt-5">Crear perfil</h1>
+                    <form onSubmit={createContact}>
+                        <div className="form-group">
+                            <label>Nombre</label>
+                            <input
+                                type="text"
+                                onChange={handleChange}
+                                className="form-control"
+                                placeholder="Añada aquí su nombre"
+                                name="nombre"
+                            />
+                        </div>
+                        <div className="form-group">
+                            <label>Apellidos</label>
+                            <input
+                                type="text"
+                                onChange={handleChange}
+                                className="form-control"
+                                placeholder="Añada aquí sus apellidos"
+                                name="apellidos"
+                            />
+                        </div>
+                        <div className="form-group">
+                            <label>Telefono</label>
+                            <input
+                                type="tel"
+                                onChange={handleChange}
+                                className="form-control"
+                                placeholder="Añada aquí el telefono"
+                                name="telefono"
+                            />
+                        </div>
 
-                    <div className="form-group">
-                        <label>Email</label>
-                        <input
-                            type="email"
-                            onChange={handleChange}
-                            className="form-control"
-                            placeholder="Añada aquí su Email"
-                            name="email" />
-                    </div>
+                        <div className="form-group">
+                            <label>Email</label>
+                            <input
+                                type="email"
+                                onChange={handleChange}
+                                className="form-control"
+                                placeholder="Añada aquí su Email"
+                                name="email" />
+                        </div>
 
-                    <div className="form-group">
-                        <label>Comunidad Autónoma</label>
-                        <input
-                            type="text"
-                            onChange={handleChange}
-                            className="form-control"
-                            placeholder="Añada aquí su Comunidad Autónoma"
-                            name="com_autonoma"
-                        />
+                        <div className="form-group">
+                            <label>Comunidad Autónoma</label>
+                            <input
+                                type="text"
+                                onChange={handleChange}
+                                className="form-control"
+                                placeholder="Añada aquí su Comunidad Autónoma"
+                                name="com_autonoma"
+                            />
 
-                    </div>
+                        </div>
 
-                    <div className="form-group">
-                        <label>Provincia</label>
-                        <input
-                            type="text"
-                            onChange={handleChange}
-                            className="form-control"
-                            placeholder="Añada aquí su provincia"
-                            name="provincia"
-                        />
-                    </div>
+                        <div className="form-group">
+                            <label>Provincia</label>
+                            <input
+                                type="text"
+                                onChange={handleChange}
+                                className="form-control"
+                                placeholder="Añada aquí su provincia"
+                                name="provincia"
+                            />
+                        </div>
 
-                    <div className="form-group">
-                        <label>Codigo Postal</label>
-                        <input
-                            type="text"
-                            onChange={handleChange}
-                            className="form-control"
-                            placeholder="Añada aquí su codigo postal"
-                            name="codigo_postal"
-                        />
-                    </div>
+                        <div className="form-group">
+                            <label>Codigo Postal</label>
+                            <input
+                                type="text"
+                                onChange={handleChange}
+                                className="form-control"
+                                placeholder="Añada aquí su codigo postal"
+                                name="codigo_postal"
+                            />
+                        </div>
 
-                    <div className="form-group">
-                        <label>Dirección</label>
-                        <input
-                            type="text"
-                            onChange={handleChange}
-                            className="form-control"
-                            placeholder="Añada aquí su dirección"
-                            name="dirección"
-                        />
-                    </div>
+                        <div className="form-group">
+                            <label>Dirección</label>
+                            <input
+                                type="text"
+                                onChange={handleChange}
+                                className="form-control"
+                                placeholder="Añada aquí su dirección"
+                                name="dirección"
+                            />
+                        </div>
 
-                    <div className="form-group">
-                        <label>Sexo</label>
-                        <input
-                            type="text"
-                            onChange={handleChange}
-                            className="form-control"
-                            placeholder="Añada aquí si es usted mujer, hombre, o no quiere definirse."
-                            name="sexo"
-                        />
-                    </div>
+                        <div className="form-group">
+                            <label>Sexo</label>
+                            <input
+                                type="text"
+                                onChange={handleChange}
+                                className="form-control"
+                                placeholder="Añada aquí si es usted mujer, hombre, o no quiere definirse."
+                                name="sexo"
+                            />
+                        </div>
 
-                    <button type="submit" className="btn btn-primary form-control mt-3">
-                        Registro
-                    </button>
-                </form>
+                        <button type="submit" className="btn btn-primary form-control mt-3">
+                            Registro
+                        </button>
+                    </form>
+                </div>
             </div>
         </div>
     );
