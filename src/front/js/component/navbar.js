650e222ba75601f89b3c8e31d327bdc9e2d7a669
DCardigo <123099651+DCardigo@users.noreply.github.com>
2023-08-17 10:06:31 +0000

    navbar ok

--- src/front/js/component/navbar.js
+++ src/front/js/component/navbar.js
@@ -3,15 +3,56 @@ import { Link } from "react-router-dom";
 
 export const Navbar = () => {
 	return (
-		<nav className="navbar navbar-light bg-light">
-			<div className="container">
-				<Link to="/">
-					<span className="navbar-brand mb-0 h1">React Boilerplate</span>
-				</Link>
-				<div className="ml-auto">
-					<Link to="/demo">
-						<button className="btn btn-primary">Check the Context in action</button>
-					</Link>
+		<nav className="navbar navbar-expand-lg navbar-light bg-light ">
+			<div className="container-fluid">
+				<a className="navbar-brand" href="#">
+					<img src="https://media.istockphoto.com/id/1023035296/es/vector/ensalada-icono-vector-de-se%C3%B1al-y-s%C3%ADmbolo-aisladas-sobre-fondo-blanco-el-concepto-de-logotipo.jpg?s=612x612&w=0&k=20&c=HMLUg1UhDlldPBK_ZNc9XBm9a5nJtbJrDk3JCj9qD30=" alt="" width="50" height="50"/>
+				</a>
+				<div className="collapse navbar-collapse " id="navbarSupportedContent">
+					<ul className="navbar-nav me-auto mb-2 mb-lg-0 ">
+
+						<li className="nav-item">
+							<a className="nav-link active" aria-current="page" href="#">Ir a mi huerto</a>
+						</li>
+						<li className="nav-item" id="btn-home">
+							<a className="nav-link active" aria-current="page" href="#">Home</a>
+						</li>
+
+						<li className="nav-item" id="btn-login">
+						<a className="nav-link active" aria-current="page" href="#">Log in</a>
+						</li>
+						
+
+						{/* FAVORITOS */}
+
+						<li className="nav-item dropdown">
+						<a className="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+							Favoritos
+						</a>
+						<ul className="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
+							<li><a className="dropdown-item" href="#">Action</a></li>
+							<li><a className="dropdown-item" href="#">Another action</a></li>
+							<li><hr className="dropdown-divider"/></li>
+							<li><a className="dropdown-item" href="#">Something else here</a></li>
+						</ul>
+						</li>
+
+						{/* CARRITO */}
+						
+						<li className="nav-item dropdown">
+							<a className="nav-link dropdown-toggle" href="#" id="carrito" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+								<i className="fa-solid fa-cart-arrow-down"></i>
+							</a>
+							<ul className="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
+								<li><a className="dropdown-item" href="#">Action</a></li>
+								<li><a className="dropdown-item" href="#">Another action</a></li>
+								<li><hr className="dropdown-divider"/></li>
+								<li><a className="dropdown-item" href="#">Something else here</a></li>
+							</ul>
+						</li>
+						
+					</ul>
+			
 				</div>
 			</div>
 		</nav>
