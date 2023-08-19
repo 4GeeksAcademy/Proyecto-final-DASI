562f898225c021f469e94768c63d4f6a0091dc0f
Sergio Reverte <129745342+Reverte10@users.noreply.github.com>
2023-08-19 17:40:21 +0000

    terminado el fetch en flux

--- src/api/models.py
+++ src/api/models.py
@@ -2,6 +2,7 @@ from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
 
+
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(120), unique=True, nullable=False)
@@ -9,7 +10,7 @@ class User(db.Model):
     is_active = db.Column(db.Boolean(), unique=False, nullable=False)
 
     def __repr__(self):
-        return f'<User {self.email}>'
+        return f"<User {self.email}>"
 
     def serialize(self):
         return {
