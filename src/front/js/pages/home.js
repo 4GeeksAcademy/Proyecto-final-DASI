cc84a3a87a137c5c09b89666110d48a12ab3fcc5
Daniel Carrión Martínez <123099651+DCardigo@users.noreply.github.com>
2023-08-25 17:53:03 +0000

    final commit

--- src/front/js/pages/home.js
+++ src/front/js/pages/home.js
@@ -1,6 +1,6 @@
 import React, { useState , useContext } from "react";
 import { Context } from "../store/appContext";
-import { Card } from "../component/card.home"
+import { Card } from "../component/card.home.js"
 import "../../styles/home.css";
 
 export const Home = () => {
