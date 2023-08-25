5a0cb1beb0499f1ece3bdf4d7f624c6ee35ae895
Inti Luna Aviles <inti.luna.aviles@gmail.com>
2023-08-25 22:25:45 +0000

    add map in front component pero no ok

--- src/front/js/component/mapa.home.js
+++ src/front/js/component/mapa.home.js
@@ -14,11 +14,7 @@ export const Mapa = () => {
                 attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
                 url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
             />
-            <Marker position={[51.505, -0.09]}>
-                <Popup>
-                    A pretty CSS3 popup. <br /> Easily customizable.
-                </Popup>
-            </Marker>
+
         </MapContainer>
     )
 
