54633310c7535f9af62df0bd8cb772b24c18b8eb
Inti Luna Aviles <inti.luna.aviles@gmail.com>
2023-08-30 11:13:02 +0000

    ajustar endpoint registro para cargar multiples registros de usuario

--- src/api/models.py
+++ src/api/models.py
@@ -110,8 +110,8 @@ class PerfilProductor(db.Model):
             "problemas": self.problemas,
             "donde_encontrar": self.donde_encontrar,
             "latitud": self.latitud,
-            "longitud": self.longitud,
-            "user_id": self.user_id
+            "longitud": self.longitud
+            
             # do not serialize the password, its a security breach
         }
 
