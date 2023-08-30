3f70528c8bc49500f8c0945f2647f5dac232a0d6
Inti Luna Aviles <inti.luna.aviles@gmail.com>
2023-08-30 20:46:55 +0000

    map showing position and icon correctly

--- src/front/js/component/mapa.home.js
+++ src/front/js/component/mapa.home.js
@@ -1,14 +1,13 @@
-import React, { useEffect, useContext, useState } from "react";
-import { Link, Navigate, Route, useNavigate } from "react-router-dom";
+import React, { useContext, useState } from "react";
+import { Link } from "react-router-dom";
 import { Context } from "../store/appContext";
-import PropTypes from "prop-types";
-//import Leaflet
-//import { MapContainer, TileLayer, useMap } from 'react-leaflet' 
 import { MapContainer, TileLayer, Marker, Popup, Icon } from 'react-leaflet'
 //import url("https://unpkg.com/leaflet@1.9.3/dist/leaflet.css");
 
 
 export const Mapa = () => {
+    const { store } = useContext(Context);
+    const perfiles = store.perfil;
 
     const position = [43.3540886, -2.8421462]
 
@@ -24,11 +23,12 @@ export const Mapa = () => {
                 url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
             />
 
-            {/* <Marker position={[43.3540886, -2.8421462], { icon: myIcon }}>
+            {/* <Marker position={position},  {icon: myIcon}> */}
+            <Marker position={position}>
                 <Popup>
                     A pretty CSS3 popup. <br /> Easily customizable.
                 </Popup>
-            </Marker> */}
+            </Marker>
 
 
         </MapContainer>
