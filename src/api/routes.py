6d18647987a60d75ae9c0a79f4b9cbb032c84173
Sergio Reverte <129745342+Reverte10@users.noreply.github.com>
2023-09-02 08:19:49 +0000

    comprobacion correcto funcionamiento despues de merge

--- src/api/routes.py
+++ src/api/routes.py
@@ -48,24 +48,6 @@ def get_all_users():
     }
 
     return jsonify(response_body), 200
-#crear productoNombre
-# @api.route('/producto', methods=['POST'])
-# def save_products():
-
-#     request_body = request.get_json(force=True)
-#     for x in request_body:
-#         item = ProductoNombre(nombre= x['nombre'])
-#         db.session.add(item)
-    
-#     db.session.commit()
-
-
-#     response_body = {
-#         'msg':'ok',
-#         "results": ['Nombre de producto Created', item.serialize()]
-#     }
-
-#     return jsonify(response_body), 200
 
 
 # -------------------- CREAR PRODUCTO --------------------
@@ -89,16 +71,6 @@ def add_producto():
     
     db.session.add(producto)
     db.session.commit()
-
-    # pedido = Pedido(
-    #     user_id= request_body ['productor_id'],
-    #     # fecha_recogida= request_body['fecha_recogida'],
-    #     producto_id= producto.serialize()["id"],
-    #     cantidad_solicitada= request_body['cantidad'],
-    # )
-
-    # db.session.add(pedido)
-    # db.session.commit()
 
 
     response_body = {
@@ -157,8 +129,6 @@ def edit_product(id):
 
     
 
-   
-
 #get lista de productoNombre
 @api.route('/producto', methods=['Get'])
 def get_all_products():
@@ -172,77 +142,8 @@ def get_all_products():
 
     return jsonify(response_body), 200
 
-# #get lista de Comunidades Autonomas
-# @api.route('/ca', methods=['Get'])
-# def get_all_ca():
-
-#     ca_query = ComunidadAutonoma.query.all()
-#     results = list(map(lambda item: item.serialize(), ca_query))
-
-#     response_body = {
-#        "results": results
-#     }
-
-#     return jsonify(response_body), 200
-
-# #post lista de Comunidades Autonomas
-# @api.route('/ca', methods=['POST'])
-# def add_ca():
-
-#     request_body = request.get_json(force=True)
-
-#     for x in request_body:
-#         item = ComunidadAutonoma(name= x['name'])
-#         db.session.add(item)
-
-#     db.session.commit()
-
-
-#     response_body = {
-#         'msg':'ok',
-#         "results": ['CA Created', item.serialize()]
-#     }
-
-#     return jsonify(response_body), 200
-
-# #get lista de Provincias
-# @api.route('/provincias', methods=['Get'])
-# def get_all_provincias():
-
-#     provincia_query = Provincia.query.all()
-#     results = list(map(lambda item: item.serialize(), provincia_query))
-
-#     response_body = {
-#        "results": results
-#     }
-
-#     return jsonify(response_body), 200
-
-# #post lista de Provincias
-# @api.route('/provincias', methods=['POST'])
-# def add_provincia():
-
-#     request_body = request.get_json(force=True)
-
-#     for x in request_body:
-#         item = Provincia(name= x['name'],
-#                         comunidad_autonoma_id= x['comunidad_autonoma_id'])
-#         print(item)
-#         db.session.add(item)
-
-#     db.session.commit()
-
-
-#     response_body = {
-#         'msg':'ok',
-#         "results": ['Provincia Created', item.serialize()]
-#     }
-
-#     return jsonify(response_body), 200
-
 
 # crear usuario
-
 
 @api.route('/registro', methods=['POST'])
 def add_user():
@@ -264,60 +165,7 @@ def add_user():
 
 
 
-    #add validation
-    # atributos = ["username","password","email"]
-    
-    # for x in atributos:
-    #     if x not in request_body:
-    #         response = f'You need to specify the {x}', 400
-    #         return response
-    #if 'nombre' not in body:
-    #    raise APIException('You need to specify the nombre', status_code=400)
-
-
-    # usuario = User(username= request_body['username'],
-    #                password= request_body['password'],
-    #                email= request_body['email']
-                   
-                   
-    #                )
-    # print(usuario)
-
-    # db.session.add(usuario)
-    # db.session.commit()
-
-
-    # response_body = {
-    #     'msg':'ok',
-    #     "results": ['Usuario Created', item.serialize()]
-    # }
-
-    # return jsonify(response_body), 200
-
-# sample post to create favoritos
-# {
-#     "characters_id": null,
-#     "planets_id":1
-    
-# }
-#sample response
-# {
-#     "msg": "ok",
-#     "results": [
-#         "Favorito Created",
-#         {
-#             "characters": null,
-#             "id": 7,
-#             "planets": "Planeta de los simios",
-#             "user_id": 1
-#         }
-#     ]
-# }
-# USERS
-
 @api.route('/users', methods=['POST'])
-
-
 
 def create_user():
 
@@ -330,13 +178,7 @@ def create_user():
         return jsonify ({
             'msg':'missing parameters (email, password, are required)'
         }), 400
-    
-    # Verificamos email válido (basico)
 
-    # if "@" not in request_body['email'] or "." not in request_body['email']:
-    #     return jsonify ({
-    #         'msg':'wrong email format(check @ .)'
-    #     }), 400
 
     # Verificamos email válido (pro)
     def validar_email(email):
@@ -374,24 +216,23 @@ def create_user():
 
 # -------------------- PERFIL PRODUCTOR --------------------
 
-
-@api.route('/perfil_productor', methods=['GET'])
+@api.route('/perfil_productor_home', methods=['POST'])
 def get_all_productores():
-    # request_body = request.get_json(force=True)
+    request_body = request.get_json(force=True)
 
-    Productor_query = PerfilProductor.query.all()
+    Productor_query = PerfilProductor.query
 
-    # if request_body['selectedOptions'] and request_body['selectedOptions']['Producto']:
-    #     product_name = request_body['selectedOptions']['Producto']
-    #     Productor_query = Productor_query.filter(PerfilProductor.producto.any(nombre=product_name))
+    if request_body['selectedOptions'] and request_body['selectedOptions']['Producto']:
+        product_name = request_body['selectedOptions']['Producto']
+        Productor_query = Productor_query.filter(PerfilProductor.producto.any(nombre=product_name))
 
-    # if request_body['selectedCommunity'] and request_body['selectedCommunity']:
-    #     community_name = request_body['selectedCommunity']
-    #     Productor_query = Productor_query.filter(PerfilProductor.comunidad_autonoma == community_name)
+    if request_body['selectedCommunity'] and request_body['selectedCommunity']:
+        community_name = request_body['selectedCommunity']
+        Productor_query = Productor_query.filter(PerfilProductor.comunidad_autonoma == community_name)
     
-    # if request_body['selectedProvince'] and request_body['selectedProvince']:
-    #     province_name = request_body['selectedProvince']
-    #     Productor_query = Productor_query.filter(PerfilProductor.provincia == province_name)
+    if request_body['selectedProvince'] and request_body['selectedProvince']:
+        province_name = request_body['selectedProvince']
+        Productor_query = Productor_query.filter(PerfilProductor.provincia == province_name)
 
     results = list(map(lambda item: item.serialize(), Productor_query))
 
@@ -400,33 +241,6 @@ def get_all_productores():
     }
 
     return jsonify(response_body), 200
-
-
-# @api.route('/perfil_productor_home', methods=['POST'])
-# def get_all_productores():
-#     request_body = request.get_json(force=True)
-
-#     Productor_query = PerfilProductor.query
-
-#     if request_body['selectedOptions'] and request_body['selectedOptions']['Producto']:
-#         product_name = request_body['selectedOptions']['Producto']
-#         Productor_query = Productor_query.filter(PerfilProductor.producto.any(nombre=product_name))
-
-#     if request_body['selectedCommunity'] and request_body['selectedCommunity']:
-#         community_name = request_body['selectedCommunity']
-#         Productor_query = Productor_query.filter(PerfilProductor.comunidad_autonoma == community_name)
-    
-#     if request_body['selectedProvince'] and request_body['selectedProvince']:
-#         province_name = request_body['selectedProvince']
-#         Productor_query = Productor_query.filter(PerfilProductor.provincia == province_name)
-
-#     results = list(map(lambda item: item.serialize(), Productor_query))
-
-#     response_body = {
-#         "results": results
-#     }
-
-#     return jsonify(response_body), 200
 
 # -------------------- CREAR PERFIL PRODUCTOR --------------------
 
