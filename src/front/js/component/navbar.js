f4aaa152e1b3c5a2a381fdadc9d59e0ccc74b474
Daniel Carrión Martínez <123099651+DCardigo@users.noreply.github.com>
2023-09-05 11:49:05 +0000

    footer ok

--- src/front/js/component/navbar.js
+++ src/front/js/component/navbar.js
@@ -1,4 +1,4 @@
-import React, {useContext, useState, useEffect} from "react";
+import React, { useContext, useState, useEffect } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import axios from "axios"
 
@@ -9,197 +9,118 @@ export const Navbar = () => {
 	const { actions, store } = useContext(Context);
 	const navigate = useNavigate()
 
-	const handlerPerfil = (e)=>{
-        e.preventDefault()
+	const handlerPerfil = (e) => {
+		e.preventDefault()
 		console.log(store.is_productor);
-		store.log === false && store.is_productor === false ? navigate('/login') : 
-		store.log === true && store.is_productor === false ? navigate('/crear_perfil') :
-        navigate("/perfil") /* --> Enlace a perfil productor*/
-    }
+		store.log === false && store.is_productor === false ? navigate('/login') :
+			store.log === true && store.is_productor === false ? navigate('/crear_perfil') :
+				navigate("/perfil") /* --> Enlace a perfil productor*/
+	}
 
-	const handlerHome = (e)=>{
-        e.preventDefault()
-        navigate("/") /* --> Enlace a la home*/
-    }
+	const handlerHome = (e) => {
+		e.preventDefault()
+		navigate("/") /* --> Enlace a la home*/
+	}
 
-	const handlerLogIn = (e)=>{
+	const handlerLogIn = (e) => {
 		e.preventDefault()
 		navigate("/login")
 	}
 
-	const handlerLogOut = (e)=>{
-        e.preventDefault()
+	const handlerLogOut = (e) => {
+		e.preventDefault()
 		let logout = actions.logout()
 		if (!logout) {
 			navigate("/login")
 		}
-    }
+	}
 
 
-	
+
 
 	useEffect(() => {
 
 		actions.getProfile();
-	
+
 	}, []);
 
 
 	return (
 		<nav className="navbar navbar-expand-lg navbar-light bg-light sticky-top">
- 			<div className="container-fluid">
-			 	
+			<div className="container-fluid">
+
 				<div className="collapse navbar-collapse col-4" id="navbarSupportedContent">
 
 					<a className="navbar-brand" href="#">
-						<img src="https://media.istockphoto.com/id/1023035296/es/vector/ensalada-icono-vector-de-se%C3%B1al-y-s%C3%ADmbolo-aisladas-sobre-fondo-blanco-el-concepto-de-logotipo.jpg?s=612x612&w=0&k=20&c=HMLUg1UhDlldPBK_ZNc9XBm9a5nJtbJrDk3JCj9qD30=" alt="" width="50" height="50"/>
+						<img src="https://media.istockphoto.com/id/1023035296/es/vector/ensalada-icono-vector-de-se%C3%B1al-y-s%C3%ADmbolo-aisladas-sobre-fondo-blanco-el-concepto-de-logotipo.jpg?s=612x612&w=0&k=20&c=HMLUg1UhDlldPBK_ZNc9XBm9a5nJtbJrDk3JCj9qD30=" alt="" width="50" height="50" />
 					</a>
-					
+
 					<li className="nav-item float-start ">
 						<a className="nav-link active link-dark" aria-current="page" href="#" onClick={handlerPerfil}>Ir a mi huerto</a>
 					</li>
-				</div>	
+				</div>
 
 				<div className="col-4  d-flex justify-content-center" >
-					
+
 					<li className="nav-item ">
 						<a className="nav-link active link-dark" aria-current="page" href="#" onClick={handlerHome}>Home</a>
 					</li>
-				</div>	
-						
-					
-					
+				</div>
+
+
+
 				<div className="d-flex col-4 justify-content-end">
-						{ (store.log === false) ? 
+					{(store.log === false) ?
 
 						<li className="nav-item" id="btn-login" onClick={handlerLogIn}>
 							<a className="nav-link active link-dark" aria-current="page" href="#">Log in</a>
 						</li>
 
-						:<li className="nav-item" id="btn-login" onClick={handlerLogOut}>
+						: <li className="nav-item" id="btn-login" onClick={handlerLogOut}>
 							<a className="nav-link active link-dark" aria-current="page" href="#">Log out</a>
 						</li>
-
-				
-
 					}
 
 					{/* FAVORITOS */}
 
-					{ (store.log === true) ? 
-				
+					{(store.log === true) ?
 
-					<li className="nav-item dropdown">
-					<a className="nav-link dropdown-toggle link-dark" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-						Favoritos
-					</a>
-					<ul className="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
-						<li><a className="dropdown-item" href="#">Action</a></li>
-						<li><a className="dropdown-item" href="#">Another action</a></li>
-						<li><hr className="dropdown-divider"/></li>
-						<li><a className="dropdown-item" href="#">Something else here</a></li>
-					</ul>
-					</li>
-					
-					: null}
+
+						<li className="nav-item dropdown">
+							<a className="nav-link dropdown-toggle link-dark" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+								Favoritos
+							</a>
+							<ul className="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
+								<li><a className="dropdown-item" href="#">Action</a></li>
+								<li><a className="dropdown-item" href="#">Another action</a></li>
+								<li><hr className="dropdown-divider" /></li>
+								<li><a className="dropdown-item" href="#">Something else here</a></li>
+							</ul>
+						</li>
+
+						: null}
 
 					{/* CARRITO */}
 
-					{ (store.log === true) ? 
-					
-					<li className="nav-item dropdown">
-						<a className="nav-link dropdown-toggle link-dark" href="#" id="carrito" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-							<i className="fa-solid fa-cart-arrow-down"></i>
-						</a>
-						<ul className="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
-							<li><a className="dropdown-item" href="#">Action</a></li>
-							<li><a className="dropdown-item" href="#">Another action</a></li>
-							<li><hr className="dropdown-divider"/></li>
-							<li><a className="dropdown-item" href="#">Something else here</a></li>
-						</ul>
-					</li>
+					{(store.log === true) ?
 
-					: null}
+						<li className="nav-item dropdown">
+							<a className="nav-link dropdown-toggle link-dark" href="#" id="carrito" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+								<i className="fa-solid fa-cart-arrow-down"></i>
+							</a>
+							<ul className="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
+								<li><a className="dropdown-item" href="#">Action</a></li>
+								<li><a className="dropdown-item" href="#">Another action</a></li>
+								<li><hr className="dropdown-divider" /></li>
+								<li><a className="dropdown-item" href="#">Something else here</a></li>
+							</ul>
+						</li>
+
+						: null}
 				</div>
-				
+
 			</div>
 		</nav>
 
-		// <nav className="navbar navbar-expand-lg navbar-light bg-light sticky-top">
-		// 	<div className="container-fluid">
-		// 		<a className="navbar-brand" href="#">
-		// 			<img src="https://media.istockphoto.com/id/1023035296/es/vector/ensalada-icono-vector-de-se%C3%B1al-y-s%C3%ADmbolo-aisladas-sobre-fondo-blanco-el-concepto-de-logotipo.jpg?s=612x612&w=0&k=20&c=HMLUg1UhDlldPBK_ZNc9XBm9a5nJtbJrDk3JCj9qD30=" alt="" width="50" height="50"/>
-		// 		</a>
-				
-		// 		<div className="collapse navbar-collapse d-flex justify-content-between" id="navbarSupportedContent">
-		// 			<ul className="navbar-nav mb-2 mb-lg-0">
-
-		// 				<li className="nav-item float-start">
-		// 					<a className="nav-link active" aria-current="page" href="#" onClick={handlerPerfil}>Ir a mi huerto</a>
-		// 				</li>
-		
-		// 				<li className="nav-item" id="btn-home">
-		// 					<a className="nav-link active" aria-current="page" href="#" onClick={handlerHome}>Home</a>
-		// 				</li>
-
-						
-		// 				{ (store.log === false) ? 
-
-		// 					<li className="nav-item" id="btn-login" onClick={handlerLogIn}>
-		// 						<a className="nav-link active" aria-current="page" href="#">Log in</a>
-		// 					</li>
-
-		// 					:<li className="nav-item" id="btn-login" onClick={handlerLogOut}>
-		// 						<a className="nav-link active" aria-current="page" href="#">Log out</a>
-		// 					</li>
-
-					
-
-		// 				}
-
-		// 				{/* FAVORITOS */}
-
-		// 				{ (store.log === true) ? 
-					
-
-		// 				<li className="nav-item dropdown">
-		// 				<a className="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-		// 					Favoritos
-		// 				</a>
-		// 				<ul className="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
-		// 					<li><a className="dropdown-item" href="#">Action</a></li>
-		// 					<li><a className="dropdown-item" href="#">Another action</a></li>
-		// 					<li><hr className="dropdown-divider"/></li>
-		// 					<li><a className="dropdown-item" href="#">Something else here</a></li>
-		// 				</ul>
-		// 				</li>
-						
-		// 				: null}
-
-		// 				{/* CARRITO */}
-
-		// 				{ (store.log === true) ? 
-						
-		// 				<li className="nav-item dropdown">
-		// 					<a className="nav-link dropdown-toggle" href="#" id="carrito" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-		// 						<i className="fa-solid fa-cart-arrow-down"></i>
-		// 					</a>
-		// 					<ul className="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
-		// 						<li><a className="dropdown-item" href="#">Action</a></li>
-		// 						<li><a className="dropdown-item" href="#">Another action</a></li>
-		// 						<li><hr className="dropdown-divider"/></li>
-		// 						<li><a className="dropdown-item" href="#">Something else here</a></li>
-		// 					</ul>
-		// 				</li>
-
-		// 				: null}
-						
-						
-		// 			</ul>
-			
-
-		// 		</div> 
-		// 	</div>
-		// </nav>
 	);
 };
