7fdae339ff624393ae5134d15c5ed48fb6b0e015
Daniel Carrión Martínez <123099651+DCardigo@users.noreply.github.com>
2023-09-07 08:05:50 +0000

    manejo errores login

--- src/front/js/pages/login.js
+++ src/front/js/pages/login.js
@@ -26,6 +26,10 @@ export const Login = () => {
 			navigate('/')
 			actions.getProfile()
 			console.log(actions.getProductor())
+			// setEmail("");
+			// setPassword("");
+			// setResp("");
+			// email === "" || password === "" ? setResp("Debes rellenar todos los campos") : setResp(store.respuesta_log);
 		} else {
 			setEmail("");
 			setPassword("");
