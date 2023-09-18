b19052907edd137e53d82d9254e9047e2b90fce1
Inti Luna Aviles <inti.luna.aviles@gmail.com>
2023-09-18 11:49:48 +0000

    add page, modify layout and navbar

---
+++ src/front/js/pages/equipo.js
@@ -0,0 +1,21 @@
+import React, { useContext, useState, useEffect } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import axios from "axios"
+
+
+export const Equipo = () => {
+
+    return (
+        <div className="Container" >
+
+
+            <div className="mb-5">
+                <h2> Daniel Carrión </h2>
+            </div>
+
+
+
+
+        </div >
+    );
+};
