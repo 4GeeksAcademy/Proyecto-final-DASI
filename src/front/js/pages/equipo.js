bf41370781d27147e476027180e17e9f57a6e398
Inti Luna Aviles <inti.luna.aviles@gmail.com>
2023-09-18 14:24:41 +0000

    add equipo cards

--- src/front/js/pages/equipo.js
+++ src/front/js/pages/equipo.js
@@ -7,10 +7,43 @@ export const Equipo = () => {
 
     return (
         <div className="Container" >
-
-
-            <div className="mb-5">
-                <h2> Daniel Carrión </h2>
+            <div className="row row-cols-1 row-cols-md-4 g-5">
+                <div className="col">
+                    <div className="card">
+                        <img src="..." className="card-img-top" alt="..."/>
+                            <div className="card-body">
+                                <h5 className="card-title">Daniel Carrión</h5>
+                                <p className="card-text">This is a longer card with supporting text below.</p>
+                            </div>
+                    </div>
+                </div>
+                <div className="col">
+                    <div className="card">
+                        <img src="..." className="card-img-top" alt="..."/>
+                            <div className="card-body">
+                                <h5 className="card-title">Sergio Reverte</h5>
+                                <p className="card-text">This is a longer card with supporting text below.</p>
+                            </div>
+                    </div>
+                </div>
+                <div className="col">
+                    <div className="card">
+                        <img src="..." className="card-img-top" alt="..."/>
+                            <div className="card-body">
+                                <h5 className="card-title">Angelica Quijada</h5>
+                                <p className="card-text">This is a longer card with supporting text below.</p>
+                            </div>
+                    </div>
+                </div>
+                <div className="col">
+                    <div className="card">
+                        <img src="..." className="card-img-top" alt="..."/>
+                            <div className="card-body">
+                                <h5 className="card-title">Inti Luna</h5>
+                                <p className="card-text">This is a longer card with supporting text below.</p>
+                            </div>
+                    </div>
+                </div>
             </div>
 
 
