8e9ccc0244978f52eec9f31b5bbcbf3487a11a77
Daniel Carrión Martínez <123099651+DCardigo@users.noreply.github.com>
2023-09-22 15:56:49 +0000

    front ok con imagen

--- src/front/js/pages/recContraseña.js
+++ src/front/js/pages/recContraseña.js
@@ -6,72 +6,80 @@ import axios from "axios"
 
 export const Contraseña = () => {
 
-	const { store, actions } = useContext(Context);
-	const navigate = useNavigate();
-	const [email, setEmail] = useState("");
-	const [password, setPassword] = useState("");
-	const [resp, setResp] = useState("");
+    const { store, actions } = useContext(Context);
+    const navigate = useNavigate();
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const [resp, setResp] = useState("");
 
-	// const handlerNavigate = (e) => {
-	// 	e.preventDefault()
-	// 	navigate("/registro")
-	// }
-	// const recContraseña = (e) => {
-	// 	e.preventDefault()
-	// 	navigate("/registro")
-	// }
+    // const handlerNavigate = (e) => {
+    // 	e.preventDefault()
+    // 	navigate("/registro")
+    // }
+    // const recContraseña = (e) => {
+    // 	e.preventDefault()
+    // 	navigate("/registro")
+    // }
 
-	async function handlerSubmit(e) {
-		e.preventDefault()
-
-
-		}
-
-	// };
-
-	// useEffect(() => {
+    async function handlerSubmit(e) {
+        e.preventDefault()
 
 
-	// 	actions.getProfile();
+    }
+
+    // };
+
+    // useEffect(() => {
+
+
+    // 	actions.getProfile();
 
 
 
-	// }, []);
+    // }, []);
 
 
-	return (
-		<div className=" bg-success bg-opacity-25 pb-3" style={{ minHeight: '80vh' }}>
+    return (
+        <div className=" bg-success bg-opacity-25 pb-3 pt-1 d-flex" style={{ minHeight: '80vh' }}>
 
-			<h1 id="" className="display-2 mx-4 col-6" >Recupera tu contraseña</h1>
+            <div className="col-6" id="cuerpo-form">
 
-            <h3 className="fs-6 fw-bolder mx-4 col-6 mt-3" >Introduce el correo electrónico empleado en tu registro y si se encuentra en nuestra base de datos te enviaremos una nueva contraseña.</h3>
+                <h1 id="" className="display-2 mx-4 col-9" >Recupera tu contraseña</h1>
 
-			<form onSubmit={handlerSubmit} className="col-6 pb-3 mx-4">
+                <h3 className="fs-5 fw-bolder mx-4 col-9 mt-3 lh-lg" >Introduce el correo electrónico empleado en tu registro y si se encuentra en nuestra base de datos te enviaremos una nueva contraseña.</h3>
 
-            <br />
 
-				<div className="form-floating my-4">
-					<input
-						type="email"
-						value={email}
-						className="form-control"
-						placeholder="Introduzca su email"
-						onChange={e => setEmail(e.target.value)}
-					/>
-					<label htmlFor="floatingInput">Correo electrónico</label>
-				</div>
-				
-                
+                <form onSubmit={handlerSubmit} className="col-11 pb-3 mx-4">
 
-				{resp}
+                    <br />
 
-				<button type="submit" id="rec" className="btn btn-submit mt-3 form-control col-4 fw-bold text-white fs-5 mb-3">
-					Recuperar contraseña
-				</button>
+                    <div className="form-floating my-3">
+                        <input
+                            type="email"
+                            value={email}
+                            className="form-control"
+                            placeholder="Introduzca su email"
+                            onChange={e => setEmail(e.target.value)}
+                        />
+                        <label htmlFor="floatingInput">Correo electrónico</label>
+                    </div>
 
-				<br/>
 
-			</form>
-		</div>
-	);
+
+                    {resp}
+
+                    <button type="submit" id="rec" className="btn btn-submit mt-3 form-control col-4 fw-bold text-white fs-5 mb-3">
+                        Recuperar contraseña
+                    </button>
+
+                    <br />
+
+                </form>
+
+            </div>
+            <div className="col-6">
+                <img id="plantas" src="https://cdn.pixabay.com/photo/2021/11/25/18/46/leaf-6824367_960_720.png" />
+            </div>
+        </div>
+    );
 };
