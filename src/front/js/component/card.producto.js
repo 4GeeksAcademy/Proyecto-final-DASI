8220ea6aca55de8b50665d03a24fa649d8a8060a
Angélica Quijada <129020174+AngeZambrano@users.noreply.github.com>
2023-09-26 02:36:46 +0000

    Arreglo de perfil

--- src/front/js/component/card.producto.js
+++ src/front/js/component/card.producto.js
@@ -23,7 +23,7 @@ export const CardProducto = () => {
     }
     return (
 
-        <div className="mt-5 mx-5 border border-success border-3 rounded p-3 overflow-x-auto" >
+        <div className="mt-4 mx-5 border border-success border-3 rounded p-3 overflow-x-auto" >
             <div className="d-flex justify-content-between">
                 <span className="display-4 mb-4">Productos del mes</span>
                 <Link to="/add_product">
@@ -35,13 +35,13 @@ export const CardProducto = () => {
             <div className="">
                 <ul className="d-flex justify-content-evenly flex-wrap m-3">{add_producto.map((item => (
                     <li key={item.id}>
-                        <div className="card mb-3 px-5 pt-3 card-product" style={{ width: "14rem" }}>
+                        <div className="card mb-3 px-2 pt-3 card-product" style={{ width: "20rem" }}>
                             {/* <img className="card-img-top" src="https://previews.123rf.com/images/sybirko/sybirko1802/sybirko180200008/94798081-personaje-de-dibujos-animados-de-col-lechuga-iceberg-s%C3%ADmbolo-vegetal-feliz-icono-de-comida.jpg" alt="..." /> */}
-                            <div className="card-body text-center fs-5">
-                                <p className="card-text">{item.nombre}</p>
-                                <p className="card-text">{item.variedad}</p>
-                                <p className="card-text">{item.tipo_produccion}</p>
-                                <p className="card-text">Recogida:{item.recogida}</p>
+                            <div className="card-body fs-5">
+                                <h4 className="card-text text-center mb-2">{item.nombre}</h4>
+                                <p className="card-text">Variedad: {item.variedad}</p>
+                                <p className="card-text">Tipo de producción: {item.tipo_produccion}</p>
+                                <p className="card-text">Recogida: {item.recogida}</p>
                                 <p className="card-text">Precio: {item.precio} €</p>
                                 <p className="card-text">{item.unidad_medida} </p>
 
